65934b67d8686d8b110c5f44af7dfbeb0013cf3e
Ashley Avicolli <ashleyavicolli1@gmail.com>
2017-02-02 12:10:07 -0500

    snakee!

--- week 2  /empty-exampleW2Assignment/sketch.js
+++ week 2  /empty-exampleW2Assignment/sketch.js
@@ -1,7 +1,68 @@
+var num = 100;
+var num2 = 50;
+var trex = 1000;
+var zeven = 7;
+var message = "hello";
+var anotherMessage = "goodbye";
+var heyhey = "I know a boy named ROY G BIV";
+var heyyyy = "up in the sky is where he lived. His name spelled out the colors of the rainbow. This is the story of ROY G BIV"; 
+var booleanVar = true;
+var booleanVar2 = false;
+var mouthH = 40;
+var mouthW = 400;
+
+
+
 function setup() {
 
+    createCanvas(600,400);
+    background(255,0,0);
+    background(255,204,0);
+    background('red');
+    
 }
 
 function draw() {
-  
+    
+ellipse(50,50)
+
+//body 
+fill('green')
+ellipse(250,200,50,50);
+ellipse(290,200,50,50);
+ellipse(320,170,50,50)
+ellipse(210,210,50,50)
+ellipse(170,200,50,50)
+//inner body 
+fill('brown')
+ellipse(250,200,30,30);
+ellipse(290,200,30,30);
+ellipse(320,170,30,30)
+ellipse(210,210,30,30)
+ellipse(170,200,30,30)
+//head
+fill('green')
+ellipse(360,130,100,50)
+//tail 
+fill('orange')
+ellipse(135,180,30,30)
+ellipse(115,170,25,25)
+ellipse(105,160,15,15)
+//inner tail
+fill('purple')
+ellipse(135,180,15,15)
+ellipse(115,170,11,11)
+ellipse(105,160,7,7)
+//tounge
+rect(390, 140, 60, 5)
+//eyes
+fill('yellow')
+ellipse(380,125,20,10)
+//pupil
+fill('black')
+ellipse(382,127,5,2)
+
+
+//ellipse(300,200,50,50);
+
 }
